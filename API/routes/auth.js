06b0f0a4b6d4e8069f4333537bd57e7e131a2630
XiaoEbisu <tran.tramy.96@gmail.com>
2021-09-20 17:51:04 +0200

    Add Login api and JWT for secure

--- API/routes/auth.js
+++ API/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -13,6 +14,41 @@ router.post("/register", async (req, res) => {
     try {
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//LOGIN 
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({
+            username: req.body.username
+        });
+        //if .... then (&&) ....
+        !user && res.status(401).json("Wrong credentials!");
+        
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
+        const origine_password = hashedPassword.toString(CryptoJS.enc.Utf8);
+        
+        origine_password !== req.body.password && res.status(401).json("Wrong credentials!");
+        
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user._isAdmin,
+        }, process.env.JWT_SEC, {
+            expiresIn: '1d'
+        });
+        
+        const {
+            password,
+            ...others
+        } = user._doc; //document of MongoDB
+
+        res.status(200).json({...others, accessToken}); //return everything except password
     } catch (err) {
         res.status(500).json(err);
     }
